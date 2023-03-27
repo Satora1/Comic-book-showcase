72863f54f22740a92908ed297fdb3174a77b7729
Michał Jeleniewski <m123jeleniewski@gmail.com>
2023-03-27 14:04:50 +0200

    stars rating added

--- client/src/components/ComicCard.js
+++ client/src/components/ComicCard.js
@@ -1,12 +1,18 @@
 import { useState } from "react"
+
 function ComicCard({ chosenComic, loggedIn, closeModal, setShowLoginForm }) {
     const [commentContent, setCommentContent] = useState("")
+    const [stars, setStars] = useState(0)
+
     function AddComment(e) {
         e.preventDefault()
         //commentContent
-        e.target.firstChild.value = ""
+        e.target.children[1].value = ""
+        setStars(0)
         setCommentContent("")
     }
+
+
 
     return <div className="comic-card-container">
         <img className="comic-image" src={chosenComic.thumbnail.path + ".jpg"} alt="comic front page" />
@@ -18,7 +24,20 @@ function ComicCard({ chosenComic, loggedIn, closeModal, setShowLoginForm }) {
             <div className="comic-cover-artist-title">Cover Artist:</div>
             <div className="comic-cover-artist">{chosenComic.creators.items[0].name}</div>
             <div className="comic-desc">{chosenComic.description}</div>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             {loggedIn && <form onSubmit={(e) => AddComment(e)}>
+                <div className="stars">
+                    {[1, 2, 3, 4, 5].map(el => (
+                        el > stars ? (<div id={el}
+                            style={{ color: "grey" }}
+                            onClick={(e) => setStars(e.target.id)}
+                            className="fa fa-star"></div>) :
+                            (<div id={el}
+                                style={{ color: "orange" }}
+                                onClick={(e) => setStars(e.target.id)}
+                                className="fa fa-star"></div>)
+                    ))}
+                </div>
                 <input type="text" placeholder="Add Comment" onChange={(e) => setCommentContent(e.target.value)}></input>
                 <input type="submit" className="submit-comment" value="Add"></input>
             </form>
